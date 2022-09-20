53dc1cd5726cf037eb18eb05131ec0e6049f1ccd
bperezgo <bsperezgo@gmail.com>
2022-09-20 16:04:45 -0500

    feat: api prefix for the api

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn'],
   });
+  app.setGlobalPrefix('/api');
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
